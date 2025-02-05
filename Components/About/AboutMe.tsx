82e96eb96a28bbaf1b1d71c8cc80d2e482c8e01e
Alif <alifhossain56782@gmail.com>
2025-02-05 16:10:54 +0600

    about me section 1st component about me done)

--- Components/About/AboutMe.tsx
+++ Components/About/AboutMe.tsx
@@ -1,11 +1,16 @@
-
-
 const AboutMe = () => {
-    return (
-        <div className="py-20 bg-soft-white mb-20">
-            <h1>This is About Me Section</h1>
+  return (
+    <div className="py-20 bg-soft-white mb-20">
+      <div className="w-10/12 mx-auto">
+        <h1 className="text-6xl font-bold font-title">About Me </h1>
+        <div className="border-l-4 mt-10 border-[#07D2FF] pl-6">
+            <p className="text-2xl font-text tracking-wide py-2 ">
+            I love building clean and functional websites using React, Tailwind, and Next.js. With 2 years of experience, I’ve worked with clients and personal projects, gaining skills in MERN backend, WordPress, and UI/UX design. I also create social media content, ads, and posters to boost online presence. 
+            </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AboutMe;
